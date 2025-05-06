722df6aff2aadeceac85ed61e9530ed4377ba199
Jyoti Kumari <68436635+Jyoti-kumari55@users.noreply.github.com>
2025-05-06 16:19:38 +0530

    chnged path

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='bg-white'>
      <div className='sidesContainer pb-1'>
         <h1 className='display-5 fw-semibold ' >
-         <Link to="/" className='link-underline link-underline-opacity-0' >
+         <Link to="/landing" className='link-underline link-underline-opacity-0' >
           <span className='primaryTextClr'>My</span>
           <span className='text-black'>Website</span>
          </Link>            
